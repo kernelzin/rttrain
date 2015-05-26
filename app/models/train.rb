db82dce03adbb5d1283c34538770ff14f33641f7
Rafael Coutinho <k@fireho.com>
2015-05-26 12:58:47 -0300

    callbacks font_properties

--- app/models/train.rb
+++ app/models/train.rb
@@ -8,6 +8,20 @@ class Train
 
   accepts_nested_attributes_for :fonts, allow_destroy: true
 
+  after_create :create_font_properties
+
+  after_update :update_font_properties
+
+  def font_properties
+    "/tmp/#{name}/font_properties"
+  end
+
+  def update_font_properties
+    if File.exist?(font_properties)
+      File.delete(font_properties)
+      create_font_properties
+  end
+
   def create_font_properties
     File.open "/tmp/#{name}/font_properties", 'w' do |file|
       fonts.each do |f|
